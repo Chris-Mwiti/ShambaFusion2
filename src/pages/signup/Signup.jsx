76ded761d841d15aacf0c496bd8007b27eb091ce
Kimani-Dominic <muiruridominic001@gmail.com>
2024-10-10 17:50:54 +0300

    fix(app): Login & signup pages

--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -3,28 +3,49 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import SignupImage from '../../assets/signup.jpg';
 import { empty } from "../../utils/empty";
+import axiosInstance from "../../API";
 
 function Signup() {
 
-    const [fullName, setFullName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [emailAddress, setEmailAddress] = useState('');
+    const [username, setusername] = useState('');
+    // const [phoneNumber, setPhoneNumber] = useState('');
+    const [email, setemail] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
+    const [confirm_password, setconfirm_password] = useState('');
+    const [isFarmer, setIsFarmer] = useState(false);
+    const [isBuyer, setIsBuyer] = useState(false);
     const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
-    const handleSignup = (e) => {
+    const handleSignup = async (e) => {
         e.preventDefault();
 
-        if(empty(fullName) || empty(phoneNumber) || empty(emailAddress) || empty(password) || empty(confirmPassword)) {
+        if(empty(username) || empty(email) || empty(password) || empty(confirm_password)) {
             setError('Fields cannot be empty');
             return;
         }
-        setError('');
 
-        //API call for signup
-    } 
+        if (password !== confirm_password ) {
+            setError('Passwords do not match');
+            return;
+        }
+        setError(null);
+        setSuccess(null);
 
+        try {
+            const response = await axiosInstance.post('/register/', {
+                username,
+                email,
+                password,
+                confirm_password,
+                is_farmer: isFarmer,
+                is_buyer: isBuyer
+            });
+            setSuccess('Signup successful!');
+        } catch (err) {
+            setError(err.response?.data?.detail || 'An error occurred during signup');
+        }
+    };
 
     return (
         <>
@@ -40,20 +61,21 @@ function Signup() {
                     </div>
                     {/* Form Section */}
                     <div className="flex-1 p-8">
-                        <h2 className="text-3xl font-bold text-center mb-6">Welcome,</h2>
+                        <h2 className="text-3xl font-bold text-center mb-6">Welcome ;) </h2>
                         <form action="" method="" className="space-y-4">
                             <div>
                                 <input 
                                     type="text" 
-                                    name="fullName" 
+                                    name="username" 
+                                    label="Full Name"
                                     placeholder="Full Name" 
                                     className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                                     required 
-                                    value={fullName}
-                                    onChange={e => setFullName(e.target.value)}
+                                    value={username}
+                                    onChange={e => setusername(e.target.value)}
                                 />
                             </div>
-                            <div>
+                            {/* <div>
                                 <input 
                                     type="tel" 
                                     name="phoneNumber" 
@@ -63,22 +85,24 @@ function Signup() {
                                     value={phoneNumber}
                                     onChange={e => setPhoneNumber(e.target.value)}
                                 />
-                            </div>
+                            </div> */}
                             <div>
                                 <input 
                                     type="email" 
-                                    name="emailAddress" 
+                                    name="email" 
+                                    label="Email"
                                     placeholder="Email address..." 
                                     className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                                     required 
-                                    value={emailAddress}
-                                    onChange={e => setEmailAddress(e.target.value)}
+                                    value={email}
+                                    onChange={e => setemail(e.target.value)}
                                 />
                             </div>
                             <div>
                                 <input 
                                     type="password" 
                                     name="password" 
+                                    label="Password"
                                     placeholder="Password..." 
                                     className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                                     required 
@@ -89,13 +113,34 @@ function Signup() {
                             <div>
                                 <input 
                                     type="password" 
-                                    name="confirmPassword" 
+                                    name="confirm_password" 
+                                    label="Confirm Password"
                                     placeholder="Confirm Password..." 
                                     className="block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" 
                                     required 
-                                    value={confirmPassword}
-                                    onChange={e => setConfirmPassword(e.target.value)}
+                                    value={confirm_password}
+                                    onChange={e => setconfirm_password(e.target.value)}
                                 />
+                            </div>
+                            <div>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={isFarmer}
+                                        onChange={() => setIsFarmer((prev) => !prev)}
+                                    />
+                                    Is Farmer
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <input
+                                        type="checkbox"
+                                        checked={isBuyer}
+                                        onChange={() => setIsBuyer((prev) => !prev)}
+                                    />
+                                    Is Buyer
+                                </label>
                             </div>
                             <div>
                                 <button 
@@ -103,7 +148,7 @@ function Signup() {
                                     className="w-full bg-indigo-600 text-white p-2 rounded-md hover:bg-indigo-700 transition duration-300"
                                     onClick={handleSignup}
                                 >
-                                    Login
+                                    Sign Up
                                 </button>
                             </div>
                             
@@ -117,6 +162,7 @@ function Signup() {
                     </div>    
                 </div>
                 {error && <p className="text-red-500 text-center">{error}</p>}
+                {success && <p className="text-green-500 text-center">{success}</p>}
             </div>   
         </>  
     ); 
